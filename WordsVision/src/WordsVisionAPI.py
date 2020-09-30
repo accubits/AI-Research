bd0399ba4779d90b20923409241872d99095e168
Alex John <alex@accubits.com>
2020-09-30 11:45:14 +0530

    updates

--- WordsVision/src/WordsVisionAPI.py
+++ WordsVision/src/WordsVisionAPI.py
@@ -12,7 +12,7 @@ def serve_pil_image(pil_img):
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
 
-@app.route('/wordsvision', methods=['POST'])
+@app.route('/imagegen', methods=['POST'])
 def genImage():
     response = request.json
     caption = response['caption']
